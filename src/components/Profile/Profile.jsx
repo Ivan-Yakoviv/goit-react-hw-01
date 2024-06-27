99128e94e400f029f2bb18208b212bb5f4f73f8b
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-06-27 23:28:51 +0300

    add Profile.module.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,37 @@
+// import clsx from 'clsx';
+import s from './Profile.module.css';
+
 const Profile = ({ name, tag, location, image, stats }) => {
     return (
-        <div>
+        <section>
             <div>
-                <img
+                <div className={s.imgWrap}>
+                    <img className={s.img}
                     src={image}
                     alt="User avatar"
-                />
-                <p>{name}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
+                    />
+                </div>
+                <div>
+                    <p>{name}</p>
+                    <p>@{tag}</p>
+                    <p>{location}</p>
+                </div>
+                <ul>
+                    <li>
+                        <span>Followers</span>
+                        <span>{stats.followers}</span>
+                    </li>
+                    <li>
+                        <span>Views</span>
+                        <span>{stats.views}</span>
+                    </li>
+                    <li>
+                        <span>Likes</span>
+                        <span>{stats.likes}</span>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <span>Followers</span>
-                    <span>{stats.followers}</span>
-                </li>
-                <li>
-                    <span>Views</span>
-                    <span>{stats.views}</span>
-                </li>
-                <li>
-                    <span>Likes</span>
-                    <span>{stats.likes}</span>
-                </li>
-            </ul>
-        </div>
+        </section>
     );
 };
 
